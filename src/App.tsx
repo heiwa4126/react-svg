f496357bc40ef8bbd2cc125fdcae13effb440dd5
heiwa4126 <heiwa4126@gmail.com>
2024-09-10 15:36:53 +0900

    add App3

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 import App1 from "./App1";
 import App2 from "./App2";
+import App3 from "./App3";
 
 function App() {
 	return (
 		<>
 			<Routes>
-				<Route path="/" element={<App2 />} />
-				<Route path="/index.html" element={<App2 />} />
+				<Route path="/" element={<App3 />} />
+				<Route path="/index.html" element={<App3 />} />
 				{/* ↑デフォルトアプリ */}
 				<Route path="/1" element={<App1 />} />
 				<Route path="/2" element={<App2 />} />
+				<Route path="/3" element={<App3 />} />
 			</Routes>
 			<Links />
 		</>
@@ -26,6 +28,9 @@ export function Links() {
 			</li>
 			<li>
 				<Link to="/2">SVGを画像にオーバーレイしてみる</Link>
+			</li>
+			<li>
+				<Link to="/3">SVGを画像にオーバーレイしてみる(2)</Link>
 			</li>
 		</ol>
 	);
---
+++ src/App3.tsx
@@ -0,0 +1,119 @@
+import "./App.css";
+
+import Qr5 from "./assets/qr5.jpg";
+
+type Position = {
+	topLeft: { x: number; y: number };
+	topRight: { x: number; y: number };
+	bottomRight: { x: number; y: number };
+	bottomLeft: { x: number; y: number };
+};
+
+type Data = {
+	text: string;
+	position: Position;
+};
+
+/** ZXingでQRコードをスキャンした結果だけ抜き出したもの */
+const data1: Data[] = [
+	{
+		text: "8004030045006",
+		position: {
+			topLeft: { x: 2322, y: 517 },
+			topRight: { x: 2057, y: 390 },
+			bottomRight: { x: 2228, y: 315 },
+			bottomLeft: { x: 2485, y: 429 },
+		},
+	},
+	{
+		text: "7501031311309",
+		position: {
+			topLeft: { x: 1134, y: 1080 },
+			topRight: { x: 801, y: 1323 },
+			bottomRight: { x: 444, y: 1107 },
+			bottomLeft: { x: 795, y: 894 },
+		},
+	},
+	{
+		text: "4006381333931",
+		position: {
+			topLeft: { x: 2127, y: 1296 },
+			topRight: { x: 2034, y: 1632 },
+			bottomRight: { x: 1518, y: 1461 },
+			bottomLeft: { x: 1674, y: 1155 },
+		},
+	},
+];
+
+function SvgQr({ item }: { item: Data }) {
+	const p = item.position;
+	return (
+		<>
+			<polygon
+				points={`
+						${p.topLeft.x},${p.topLeft.y}
+						${p.topRight.x},${p.topRight.y}
+						${p.bottomRight.x},${p.bottomRight.y}
+						${p.bottomLeft.x},${p.bottomLeft.y}
+						${p.topLeft.x},${p.topLeft.y}
+					`}
+				fill="rgba(0, 255, 0, 0.2)"
+				stroke="none"
+				strokeWidth="10"
+			/>
+			<text
+				font-family="Arial"
+				font-size="60"
+				text-anchor="middle"
+				x={(p.topLeft.x + p.bottomRight.x) / 2}
+				y={(p.topLeft.y + p.bottomRight.y) / 2 + 30}
+				fill="Red"
+			>
+				{item.text}
+			</text>
+		</>
+	);
+}
+
+function SvgX({
+	width,
+	height,
+	data,
+}: { width: number; height: number; data: Data[] }) {
+	return (
+		<svg
+			style={{
+				position: "absolute",
+				top: 0,
+				left: 0,
+				width: "100%",
+				height: "100%",
+			}}
+			viewBox={`0 0 ${width} ${height}`}
+			preserveAspectRatio="none"
+		>
+			<title>svg</title>
+			{data.map((item, index) => (
+				<SvgQr item={item} key={`${index}${item.text}`} />
+			))}
+		</svg>
+	);
+}
+
+function App() {
+	return (
+		<>
+			<h1>3. SVGを画像にオーバーレイしてみる (2)</h1>
+			<div style={{ position: "relative", width: "fit-content" }}>
+				<img
+					src={Qr5}
+					alt="QRCode Example No.5 (3024 x 1935)"
+					style={{ display: "block", width: "800px", height: "auto" }}
+				/>
+				<SvgX width={3024} height={1935} data={data1} />
+			</div>
+		</>
+	);
+}
+
+export default App;
