20fbb5902be4e49aeebc9e6e4f1561c74c0b9add
heiwa4126 <heiwa4126@gmail.com>
2024-09-10 13:35:33 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { SVG, type Svg } from "@svgdotjs/svg.js";
 import "./App.css";
+import SvgEx3 from "./assets/svgex3.svg";
 
 /**
  * サンプル用にバングラデシュの国旗みたいなSVGを作る関数
@@ -54,14 +55,69 @@ function SvgComponent({ src }: { src: Svg }) {
 	return <div dangerouslySetInnerHTML={{ __html: src.svg() }} />;
 }
 
+/**
+ * getSvgEx1()と同じSVGをJSXで記述したもの。コピペじゃダメでちょっと修正が必要
+ * @returns　SVGを描画するコンポーネント
+ */
+function SvgEx2() {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			version="1.1"
+			xmlnsXlink="http://www.w3.org/1999/xlink"
+			width="320"
+			height="180"
+		>
+			<title>SVG Example 2</title>
+			<rect width="320" height="180" fill="#00ff00" />
+			<circle r="50" cx="160" cy="90" fill="#ff0000" />
+			<text
+				font-family="Arial"
+				font-size="45"
+				text-anchor="middle"
+				fill="#0000ff"
+				data-svgjs="{&quot;leading&quot;:&quot;1.5em&quot;}"
+				x="160"
+				y="105.5"
+			>
+				<tspan dy="0" x="160">
+					Hello, JSX!
+				</tspan>
+			</text>
+			<text
+				font-family="Consolas, Menlo, monospace"
+				font-size="30"
+				text-anchor="middle"
+				fill="#ffffff"
+				data-svgjs="{&quot;leading&quot;:&quot;1em&quot;}"
+				x="160"
+				y="136"
+			>
+				<tspan dy="0" x="160">
+					monospace font
+				</tspan>
+			</text>
+		</svg>
+	);
+}
+
 function App() {
 	const svg1 = getSvgEx1();
 
 	return (
 		<>
 			<h1>1. 動的にSVGを作って描画する</h1>
-			<p>SVGは変化しないものとする</p>
+			<h2>SVG文字列を生成して dangerouslySetInnerHTML で埋め込む</h2>
 			<SvgComponent src={svg1} />
+			<p>生でSVGを生成するよりはすこし楽</p>
+			<h2>JSX/TSXコンポーネントとして扱う</h2>
+			<SvgEx2 />
+			<p>一番Reactっぽいけどめんどくさい。他のフレームワークに応用できない</p>
+			<h2>imgのソースファイル</h2>
+			<div>
+				<img src={SvgEx3} alt="SVG Ex3" />
+			</div>
+			<p>動的じゃない</p>
 		</>
 	);
 }
