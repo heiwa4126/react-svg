d0c1cbb03c4d8f27c48b0fda98ba61ecc7af7b41
heiwa4126 <heiwa4126@gmail.com>
2024-10-15 14:46:53 +0900

    [WIP] backup

--- src/App7.tsx
+++ src/App7.tsx
@@ -1,36 +1,57 @@
 import { useEffect, useRef, useState } from "react";
 
+const isOverlapping = (rect1: SVGRect, rect2: SVGRect): boolean => {
+	return !(
+		rect1.x + rect1.width < rect2.x ||
+		rect2.x + rect2.width < rect1.x ||
+		rect1.y + rect1.height < rect2.y ||
+		rect2.y + rect2.height < rect1.y
+	);
+};
+
 const SvgTextComponent = ({ texts }: { texts: string[] }) => {
-	const [yPositions, setYPositions] = useState<number[]>([]);
+	const [yPositions, setYPositions] = useState<number[]>(new Array(texts.length).fill(20));
 	const textRefs = useRef<(SVGTextElement | null)[]>([]);
 
 	useEffect(() => {
-		// 各textのバウンディングボックスを取得し、y座標を計算
 		if (textRefs.current.length > 0) {
 			const yPosArray: number[] = [];
-			let currentY = 20; // 最初のテキストの初期y座標
-
-			textRefs.current.forEach((ref, index) => {
-				if (ref) {
-					const bbox = ref.getBBox();
-					yPosArray[index] = currentY;
-					currentY += bbox.height; // 次のテキストのy座標を更新
+			const bboxes = textRefs.current.map((ref) => ref?.getBBox());
+			console.log({ bboxes });
+			for (let i = 1; i < bboxes.length; i++) {
+				const currentBBox = bboxes[i];
+				const prevBBox = bboxes[i - 1];
+				if (currentBBox && prevBBox) {
+					if (isOverlapping(currentBBox, prevBBox)) {
+						currentBBox.y += prevBBox.height;
+					}
 				}
-			});
+			}
+			console.log({ bboxes });
+			const offsetY = yPositions[0] - bboxes[0]!.y;
+			for (const bbox of bboxes) {
+				if (bbox) {
+					yPosArray.push(bbox.y + offsetY); // アンカーが何処にあるかによって変わる
+				}
+			}
 			setYPositions(yPosArray);
 		}
 	}, []);
 
 	return (
-		<svg width="400" height="100">
+		<svg width="400" height="120">
 			<title>SvgTextComponent</title>
+			<rect x="0" y="0" width="400" height="120" fill="lightgray" />
 			{texts.map((text, index) => (
 				<text
 					key={`${index}${text}`}
 					// biome-ignore lint/suspicious/noAssignInExpressions: <explanation>
 					ref={(el) => (textRefs.current[index] = el)}
-					x={60 * index} // 各テキストを少し右にずらす例
-					y={yPositions[index] || 0} // 計算したy座標を適用
+					x={20 * index} // 各テキストを少し右にずらす例
+					y={yPositions[index]} // 計算したy座標を適用
+					fill="black"
+					textAnchor="left"
+					fontSize="16"
 				>
 					{text}
 				</text>
@@ -43,7 +64,7 @@ function App() {
 	return (
 		<>
 			<h1>7. 重なるテキストをずらす</h1>
-			<SvgTextComponent texts={["Hello", "World", "React", "SVG"]} />
+			<SvgTextComponent texts={["*", "Hello", "World", "React", "SVG"]} />
 		</>
 	);
 }
